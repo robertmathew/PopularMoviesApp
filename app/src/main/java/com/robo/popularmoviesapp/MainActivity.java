c93e1994d0d706edd53912f41c335344d3d88013
robo <robu1691@gmail.com>
2015-11-20 19:51:26 +0530

    Removed unnecessary  code

--- app/src/main/java/com/robo/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/robo/popularmoviesapp/MainActivity.java
@@ -9,8 +9,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter.addFragment(PopularMoviesFragment.newInstance(SORT_POPULAR), "MOST POPULAR");
+        mSectionsPagerAdapter.addFragment(RatingMoviesFragment.newInstance(SORT_RATING), "TOP RATING");
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
@@ -52,29 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main2, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void switchToDetail(Intent intent){
         startActivity(intent);
     }
@@ -83,40 +63,33 @@ public class MainActivity extends AppCompatActivity {
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    static class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            switch (position){
-                case 0:
-                    return PopularMoviesFragment.newInstance(SORT_POPULAR);
-                case 1:
-                    return PopularMoviesFragment.newInstance(SORT_RATING);
-                default:
-                    return null;
-            }
+            return mFragments.get(position);
         }
 
         @Override
         public int getCount() {
-            // Show 2 total pages.
-            return 2;
+            return mFragments.size();
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "MOST POPULAR";
-                case 1:
-                    return "TOP RATING";
-            }
-            return null;
+            return mFragmentTitles.get(position);
         }
     }
 }
--- app/src/main/java/com/robo/popularmoviesapp/RatingMoviesFragment.java
+++ app/src/main/java/com/robo/popularmoviesapp/RatingMoviesFragment.java
@@ -17,21 +17,21 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PopularMoviesFragment extends Fragment {
+public class RatingMoviesFragment extends Fragment {
 
-    private final String LOG_TAG = PopularMoviesFragment.class.getSimpleName();
+    private final String LOG_TAG = RatingMoviesFragment.class.getSimpleName();
 
-    private static final String SORT_POPULAR = "popularity.desc";
+    private static final String SORT_RATING = "vote_average.desc";
 
     private String sortOrder;
-    private static final String POPULAR_LIST = "popularList";
+    private static final String RATING_LIST = "ratingList";
 
     ProgressBar mProgressBar;
-    MoviePosterAdapter mAdapter;
-    ArrayList<Movie> mPopularList = new ArrayList<>();
+    MoviePosterAdapter ratingAdapter;
+    ArrayList<Movie> mRatingList = new ArrayList<>();
 
-    public static PopularMoviesFragment newInstance(String sort) {
-        PopularMoviesFragment fragment = new PopularMoviesFragment();
+    public static RatingMoviesFragment newInstance(String sort) {
+        RatingMoviesFragment fragment = new RatingMoviesFragment();
         Bundle args = new Bundle();
         args.putString("sort", sort);
         fragment.setArguments(args);
@@ -42,50 +42,54 @@ public class PopularMoviesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sortOrder = getArguments().getString("sort");
-        Log.d(LOG_TAG, "onCreate: " + sortOrder);
 
         if (savedInstanceState != null) {
-            Log.d(LOG_TAG, "Has savedInstance");
-            mPopularList = savedInstanceState.getParcelableArrayList(POPULAR_LIST);
+            Log.d(LOG_TAG, "Has saved instance");
+            mRatingList = savedInstanceState.getParcelableArrayList(RATING_LIST);
         } else {
-            FetchPopularMovies pm = new FetchPopularMovies();
-            pm.execute(SORT_POPULAR);
+            FetchRatingMovies rm = new FetchRatingMovies();
+            rm.execute(SORT_RATING);
         }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_popular_movies, container, false);
+        View view = inflater.inflate(R.layout.fragment_rating_movies, container, false);
 
-        mAdapter = new MoviePosterAdapter(getActivity(), mPopularList);
+        ratingAdapter = new MoviePosterAdapter(getActivity(), mRatingList);
 
         mProgressBar = (ProgressBar) view.findViewById(R.id.movies_progress_bar);
         RecyclerView mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
         RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), 3, GridLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(ratingAdapter);
 
         return view;
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        outState.putParcelableArrayList(POPULAR_LIST, mPopularList);
+        outState.putParcelableArrayList(RATING_LIST, mRatingList);
         super.onSaveInstanceState(outState);
     }
 
-    public class FetchPopularMovies extends FetchMovieListTask {
+    public class FetchRatingMovies extends FetchMovieListTask {
 
         @Override
         protected void onPostExecute(ArrayList<Movie> movies) {
             //super.onPostExecute(movies);
 
-            mPopularList = movies;
-            mAdapter.setMoviesData(mPopularList);
-            mAdapter.notifyDataSetChanged();
+            mRatingList = movies;
+            /*for(Movie m : movies){
+                Log.d(LOG_TAG, "onPostExecute: " + m.getTitle());
+            }*/
+
+            ratingAdapter.setMoviesData(mRatingList);
+            ratingAdapter.notifyDataSetChanged();
             mProgressBar.setVisibility(View.GONE);
         }
     }
